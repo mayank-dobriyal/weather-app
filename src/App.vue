787194ca2f75e252476b215d4687183660a70665
mayank-dobriyal <mayankdobiyal@flairboat.com>
2022-09-16 09:40:25 +0530

    tailwind

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    yo
+<RouterView/>
+  </div>
+</template>
+
+<script>
+  import { RouterView } from 'vue-router';
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
